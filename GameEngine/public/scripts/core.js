e212494d4afb5c3789722c6b432a60a0c8508b7d
CorkCastle <carternielsen4@gmail.com>
2015-10-25 14:47:30 -0400

    Input manager and plane model
    Manager for input and plane model added.

--- GameEngine/public/scripts/core.js
+++ GameEngine/public/scripts/core.js
@@ -1,6 +1,11 @@
 //The core game module
 var core = (function(){
 
+  //Test cube rotation
+  var rotX = 0;
+  var rotY = 0;
+  var rotZ = 0;
+
   //Initialize engine
   var init = function(){
 
@@ -22,12 +27,21 @@ var core = (function(){
     //Update loop
     requestAnimFrame(core.update);
     renderer.render();
+
+    //Rotate model
+    core.rotX += 1;
+    core.rotY -= 1;
   };
 
   //Module visibility
   return {
     init: init,
     start: start,
-    update: update
+    update: update,
+
+    //Test cube rotation
+    rotX: rotX,
+    rotY: rotY,
+    rotZ: rotZ
   };
 })();
---
+++ GameEngine/public/scripts/inputManager.js
@@ -0,0 +1,21 @@
+//Store user input data
+var inputManager = (function(){
+
+  //Keyboard events
+  var keysDown = new Array(180);
+
+  //Key is pressed
+  window.onkeydown = function(e){
+    inputManager.keysDown[e.keyCode] = true;
+  };
+
+  //Key is released
+  window.onkeyup = function(e){
+    inputManager.keysDown[e.keyEvent] = false;
+  };
+
+  //Module visibility
+  return{
+    keysDown: keysDown
+  };
+})();
